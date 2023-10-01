5dadfa8314e2dc4ade3663dab2f9d78f80e3cace
Chris Brown <43240184+chrisbrown-01@users.noreply.github.com>
2023-10-01 15:03:29 -0400

    Binary search solutions

--- 0004_MedianOfTwoSortedArrays.cs
+++ 0004_MedianOfTwoSortedArrays.cs
@@ -20,6 +20,40 @@ namespace LeetCode
 
     internal class _0004_MedianOfTwoSortedArrays
     {
+        // Binary search solution
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            List<int> merged = new List<int>();
+            int i = 0, j = 0;
+
+            while (i < nums1.Length && j < nums2.Length)
+            {
+                if (nums1[i] < nums2[j])
+                {
+                    merged.Add(nums1[i++]);
+                }
+                else
+                {
+                    merged.Add(nums2[j++]);
+                }
+            }
+
+            while (i < nums1.Length) merged.Add(nums1[i++]);
+            while (j < nums2.Length) merged.Add(nums2[j++]);
+
+            int mid = merged.Count / 2;
+            if (merged.Count % 2 == 0)
+            {
+                return (merged[mid - 1] + merged[mid]) / 2.0;
+            }
+            else
+            {
+                return merged[mid];
+            }
+        }
+
+        // My first non-optimal solution
+        /*
         public double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
             var nums1List = nums1.ToList();
@@ -41,5 +75,6 @@ namespace LeetCode
                 return numsList[index];
             }
         }
+        */
     }
 }
---
+++ 0981_TimeBasedKeyValueStore.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /*
+    
+    Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+    Implement the TimeMap class:
+
+    TimeMap() Initializes the object of the data structure.
+    void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+    String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ 
+
+    Input
+    ["TimeMap", "set", "get", "get", "set", "get", "get"]
+    [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+    Output
+    [null, null, "bar", "bar", null, "bar2", "bar2"]
+
+    Explanation
+    TimeMap timeMap = new TimeMap();
+    timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+    timeMap.get("foo", 1);         // return "bar"
+    timeMap.get("foo", 3);         // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+    timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4.
+    timeMap.get("foo", 4);         // return "bar2"
+    timeMap.get("foo", 5);         // return "bar2"
+ 
+
+    Constraints:
+
+    1 <= key.length, value.length <= 100
+    key and value consist of lowercase English letters and digits.
+    1 <= timestamp <= 10^7
+    All the timestamps timestamp of set are strictly increasing.
+    At most 2 * 10^5 calls will be made to set and get.
+
+    */
+
+
+
+    internal class _0981_TimeBasedKeyValueStore
+    {
+    }
+
+    // Binary search problem
+    public class TimeMap
+    {
+        private readonly Dictionary<string, IList<(int time, string value)>> _map = new();
+
+        public void Set(string key, string value, int timestamp)
+        {
+            if (!_map.ContainsKey(key))
+            {
+                _map[key] = new List<(int, string)>();
+            }
+
+            _map[key].Add((timestamp, value));
+        }
+
+        public string Get(string key, int timestamp)
+        {
+            if (!_map.ContainsKey(key))
+            {
+                return string.Empty;
+            }
+
+            var list = _map[key];
+            var left = 0;
+            var right = list.Count - 1;
+
+            while (left <= right)
+            {
+                var mid = (right + left) / 2;
+                if (list[mid].time == timestamp)
+                {
+                    return list[mid].value;
+                }
+
+                if (list[mid].time <= timestamp)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left == 0 ? string.Empty : list[left - 1].value;
+        }
+    }
+}
